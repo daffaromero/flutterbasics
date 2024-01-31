96056801767d10413996026185fe4551d74123f8
Daffa Muhammad Romero <daffaromero@mail.ugm.ac.id>
2024-01-31 19:53:38 +0700

    Very simple variable inside an ElevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+  String buttonName = 'Clickie';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter Basics'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              print('Hello World');
+            },
+            child: Text(buttonName),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+                color: Colors.black38,
+              ),
+              label: 'Profile',
+            ),
+          ],
         ),
       ),
     );
