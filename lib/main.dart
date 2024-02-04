86eab43f397cd986c9c238c05f24108be9840478
Daffa Muhammad Romero <daffaromero@mail.ugm.ac.id>
2024-02-04 17:53:59 +0700

    feat(multiple): navigator push + pop

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'dart:ffi';
+// import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 
@@ -14,73 +14,52 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String buttonName = 'Click Me';
-  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
           title: const Text('Flutter Basics'),
         ),
         body: Center(
-            child: currentIndex == 0
-                ? Container(
-                    height: double.infinity,
-                    width: double.infinity,
-                    // color: Colors.amberAccent,
-                    decoration: const FlutterLogoDecoration(),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.amber,
-                            foregroundColor: Colors.white,
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              buttonName = 'Clicked';
-                            });
-                          },
-                          child: Text(buttonName),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              buttonName = 'Click Me';
-                            });
-                          },
-                          child: Text(buttonName),
-                        ),
-                      ],
-                    ),
-                  )
-                : Image.asset('images/963lmdh.jpg')),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.person,
-              ),
-              label: 'Profile',
-            ),
-          ],
-          currentIndex: currentIndex,
-          onTap: (int index) {
-            setState(() {
-              currentIndex = index;
-            });
+            child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (BuildContext context) {
+              return const SecondPage();
+            }));
           },
+          child: const Text("Go to Second Page"),
+        )));
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Second Page'),
         ),
-      ),
-    );
+        body: Center(
+            child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text("Go Back"),
+        )));
   }
 }
