05a18b15e547fdb95b63c3668bf41d66aa87c7fc
Daffa Muhammad Romero <daffaromero@mail.ugm.ac.id>
2024-02-05 17:31:24 +0700

    feat(profile): more Navigator stuff, GestureDetector attempt

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-// import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,48 +16,103 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FirstPage(),
+      home: MyAppExt(),
     );
   }
 }
 
-class FirstPage extends StatelessWidget {
-  const FirstPage({super.key});
+class MyAppExt extends StatefulWidget {
+  const MyAppExt({super.key});
+
+  @override
+  State<MyAppExt> createState() => _MyAppExtState();
+}
+
+class _MyAppExtState extends State<MyAppExt> {
+  String buttonName = 'Click Me';
+  int currentIndex = 0;
+  bool isClicked = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Basics'),
-        ),
-        body: Center(
-            child: ElevatedButton(
-          onPressed: () {
-            Navigator.of(context)
-                .push(MaterialPageRoute(builder: (BuildContext context) {
-              return const SecondPage();
-            }));
-          },
-          child: const Text("Go to Second Page"),
-        )));
+      appBar: AppBar(
+        title: const Text('Flutter Basics'),
+      ),
+      body: Center(
+        child: currentIndex == 0
+            ? Container(
+                height: double.infinity,
+                width: double.infinity,
+                // color: Colors.amberAccent,
+                decoration: const FlutterLogoDecoration(),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.amber,
+                        foregroundColor: Colors.white,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          buttonName = 'Clicked';
+                        });
+                      },
+                      child: Text(buttonName),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => const NextPage(),
+                          ),
+                        );
+                      },
+                      child: const Text('Next Page'),
+                    ),
+                  ],
+                ),
+              )
+            : GestureDetector(
+                onTap: () => setState(() => isClicked = !isClicked),
+                child: isClicked
+                    ? Image.network(
+                        'https://i.pinimg.com/736x/96/0a/c8/960ac856cb33b4e4a8348b41185e62ec.jpg')
+                    : Image.asset('images/963lmdh.jpg')),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+            ),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: currentIndex,
+        onTap: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
+    );
   }
 }
 
-class SecondPage extends StatelessWidget {
-  const SecondPage({super.key});
+class NextPage extends StatelessWidget {
+  const NextPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Second Page'),
-        ),
-        body: Center(
-            child: ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: const Text("Go Back"),
-        )));
+      appBar: AppBar(),
+    );
   }
 }
